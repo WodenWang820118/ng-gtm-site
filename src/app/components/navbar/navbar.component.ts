11cda2556e68c9a48cb6d1a4ab532674961ec195
Guan-Xin <alex0972112871@gmail.com>
2023-10-15 21:26:51 +0800

    feat: details and auth service 1. The details view will respond to what users select on the destination view. 2. Using json-server, the app can mimic login and logout operations.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,4 +10,10 @@ import { RouterModule } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(public authService: AuthService) {
+    this.authService.getIsLoggedIn().subscribe((isLoggedIn) => {
+      console.log('isLoggedIn', isLoggedIn);
+    });
+  }
+}
