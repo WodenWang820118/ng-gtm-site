c880230e80e6a335fac7f4cc98a6968224bf61a4
Guan-Xin <alex0972112871@gmail.com>
2023-10-15 12:21:47 +0800

    feat: adding views

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DestinationComponent } from './views/destination/destination.component';
+import { MainComponent } from './views/main/main.component';
+import { LoginComponent } from './views/login/login.component';
+import { BasketComponent } from './views/basket/basket.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'destinations', component: DestinationComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'basket', component: BasketComponent },
+  { path: '**', redirectTo: '' },
+];
