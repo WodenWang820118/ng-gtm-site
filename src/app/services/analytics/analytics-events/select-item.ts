7b952b8b1206a1a468e9fdfebfb1e9a0b646ef23
Guan-Xin <alex0972112871@gmail.com>
2023-10-18 21:35:30 +0800

    feat: select_item

---
+++ src/app/services/analytics/analytics-events/select-item.ts
@@ -0,0 +1,30 @@
+import { AnalyticsEventTracker } from '../../../models/analytics-event-tracker.model';
+
+export class SelectItemEventTracker implements AnalyticsEventTracker {
+  constructor(private eventName: string) {
+    this.eventName = eventName;
+  }
+
+  trackEvent(eventData: any): void {
+    if (!eventData) return;
+    const ecommerce = {
+      currency: 'USD',
+      value: eventData.price,
+      items: [
+        {
+          item_id: eventData.id,
+          item_name: eventData.title,
+          item_category: eventData.title,
+          price: eventData.price,
+          quantity: 1,
+        },
+      ],
+    };
+
+    window.dataLayer.push({ ecommerce: null }); // Clear the previous ecommerce object (if any
+    window.dataLayer.push({
+      event: this.eventName,
+      ecommerce,
+    });
+  }
+}
