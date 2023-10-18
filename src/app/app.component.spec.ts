0df272550e88fcb35fb5b526fb11961b8ec1baa6
Guan-Xin <alex0972112871@gmail.com>
2023-10-18 15:07:01 +0800

    feat: deployment 1. It enables the local build for gtm script insertion. 2. It it deployed on the GitHub page. 3. Update project naming. 4. Update asset paths to be relative paths.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ng-55-gtm-site' title`, () => {
+  it(`should have the 'ng-gtm-site' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ng-gtm-site');
@@ -25,7 +25,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'ng-55-ats-site app is running!'
+      'ng-gtm-site app is running!'
     );
   });
 });
