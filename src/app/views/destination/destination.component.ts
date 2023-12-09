70bce4e740ee81c33c9b8a68da55286198c29013
Guan-Xin <alex0972112871@gmail.com>
2023-12-09 21:06:02 +0800

    feat: youtube tracking 1. use Content Security Policy to allow YouTube iframe API to work 2. add angular youtube-player component 3. fix: page_view parameter

--- src/app/views/destination/destination.component.ts
+++ src/app/views/destination/destination.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { DestinationService } from '../../services/destination/destination.service';
 import { SharedService } from '../../services/shared/shared.service';
 import { WindowSizeService } from '../../services/window-size/window-size.service';
@@ -6,18 +11,29 @@ import { NavigationService } from '../../../app/services/navigation/navigation.s
 import { FormControl, FormGroup } from '@angular/forms';
 import { SharedModule } from 'src/app/shared.module';
 import { SearchService } from 'src/app/services/search/search.service';
+import { YouTubePlayerModule } from '@angular/youtube-player';
+
 @Component({
   selector: 'app-destination',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, YouTubePlayerModule],
   templateUrl: './destination.component.html',
   styleUrls: ['./destination.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class DestinationComponent {
   destinations = this.searchService.searchResults$;
   searchForm: FormGroup = new FormGroup({
     searchTerm: new FormControl(''),
   });
+
+  @ViewChild('ytPlayerModal') ytPlayerModal!: ElementRef;
+  @ViewChild('player') videoPlayer: any;
+  videoId = 'g0mwkiXIaBo';
+  showVideoPlayer = false;
+  playerVars = {
+    enablejsapi: 1,
+  };
 
   constructor(
     private destinationService: DestinationService,
@@ -49,5 +65,19 @@ export class DestinationComponent {
     const query = f.value.searchTerm;
     this.searchService.search(query);
     this.navigateToSearchResults(query);
+  }
+
+  getVideoId(url: string): string {
+    return url.split('/')[url.split('/').length - 1];
+  }
+
+  closeModal() {
+    this.showVideoPlayer = false;
+    this.videoPlayer.nativeElement.pauseVideo();
+  }
+
+  showModal(url: string) {
+    this.videoId = this.getVideoId(url);
+    this.showVideoPlayer = true;
   }
 }
