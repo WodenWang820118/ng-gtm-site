2380931a1a85dc5698d759e745b4d769656e6bfd
Guan-Xin Wang <guanxinwang0118@gmail.com>
2024-01-04 20:44:17 +0800

    fix: scroll tracking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { CookieService } from './services/cookie/cookie.service';
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewChecked,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -7,8 +13,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { DisclaimerComponent } from './components/disclaimer/disclaimer.component';
 import { AuthService } from './services/auth/auth.service';
-import { take, tap } from 'rxjs';
+import { Subject, first, take, tap } from 'rxjs';
 import { UrlTrackerService } from './services/url-tracker/url-tracker.service';
+import { LoadingService } from './services/loading/loading.service';
 
 @Component({
   selector: 'app-root',
@@ -24,13 +31,16 @@ import { UrlTrackerService } from './services/url-tracker/url-tracker.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewChecked {
   title = 'ng-gtm-site';
+  @ViewChild('loadingDiv', { static: false }) loadingDiv!: ElementRef;
+  private destroy$ = new Subject<void>();
 
   constructor(
     private cookieService: CookieService,
     private authService: AuthService,
-    private urlTrackerService: UrlTrackerService
+    private urlTrackerService: UrlTrackerService,
+    private loadingService: LoadingService
   ) {}
 
   ngOnInit() {
@@ -50,6 +60,36 @@ export class AppComponent implements OnInit {
       )
       .subscribe();
 
+    this.loadingService
+      .getLoadingState()
+      .pipe(
+        first((isLoading) => !isLoading),
+        tap((isLoading) => {
+          if (!isLoading) {
+            window.dataLayer.push({
+              event: 'componentsLoaded',
+            });
+          }
+        })
+      )
+      .subscribe();
     this.urlTrackerService.initializeUrlTracking();
+  }
+
+  ngAfterViewChecked() {
+    try {
+      if (this.loadingDiv.nativeElement) {
+        this.loadingService.setLoadingState(true);
+      }
+    } catch (error) {
+      // loadingDiv is no longer available
+      this.loadingService.setLoadingState(false);
+    }
+  }
+
+  ngOnDestroy() {
+    // Cleanup subscriptions
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
